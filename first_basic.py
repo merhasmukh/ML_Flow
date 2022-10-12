d478dd7c3458fe4d860dc30d44334fc341298c1d
mhgn <mhgn0001@gmail.com>
2022-10-12 20:43:08 +0530

    ml flow basic program added on 12 oct 2022

---
+++ first_basic.py
@@ -0,0 +1,16 @@
+import mlflow
+
+
+def calculate_nth_power(x,n):
+    return x**n
+
+
+
+if __name__=='__main__':
+    with mlflow.start_run():
+        x,n=3,6
+        y=calculate_nth_power(x,n)
+        mlflow.log_param("x",x)
+        mlflow.log_param("n",n)
+        mlflow.log_metric("y",y)
+        
